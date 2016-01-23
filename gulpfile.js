5a3ae21c77bbb8734a04092ec06036d594c4a1af
Al Carruth <carruth@volente.com>
2016-01-23 01:51:14 -0600

    modified:   dist/pizza_app/css/style.css 	modified:   gulpfile.js 	modified:   package.json 	deleted:    src/mobile_portfolio/img/.gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var mustache = require('mustache');
+var gm = require('gulp-gm');
+var compress = require('gulp-yuicompressor');
 
-gulp.task('default', function() {
+
+gulp.task('scripts', function() {
+	 gulp.src('src/pizza_app/js/*.js')
+		  .pipe(uglify())
+		  .pipe(gulp.dest('dist/pizza_app/min_js/'))
 });
+
+gulp.task('styles', function() {
+	 console.log('styles');
+});
+
+gulp.task('images', function() {
+	 console.log('images');
+});
+
+gulp.task('templates', function() {
+	 console.log('templates');
+});
+
+gulp.task('default', ['scripts', 'images', 'styles', 'templates']);
+
+
+
