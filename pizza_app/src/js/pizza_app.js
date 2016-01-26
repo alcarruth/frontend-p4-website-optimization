d402a8189b5edac8d88fa48bfffd9ffc6930ccc0
Al Carruth <carruth@volente.com>
2016-01-26 07:59:51 -0600

    modified:   pizza_app/src/js/pizza_app.js

--- pizza_app/src/js/pizza_app.js
+++ pizza_app/src/js/pizza_app.js
@@ -4,15 +4,10 @@
 function PizzaApp() {
 
 	 // adjust these to suit
-<<<<<<< HEAD
+
 	 var pizzaMenuSize = 100;
 	 var slidingPizzaRows = 6;
 	 var slidingPizzaCols = 8;
-=======
-	 var pizza_menu_size = 100;
-	 var sliding_pizza_rows = 7;
-	 var sliding_pizza_cols = 8;
->>>>>>> cf5a96807d33439ab61662cf474fb806ab3b6905
 
 	 // leave these alone :-)
 	 var pizzaDesigner = PizzaDesigner();
