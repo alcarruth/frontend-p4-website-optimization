d737ffaf60a5bc68e2ffc561c5f92b2c22f47115
Al Carruth <al.carruth@gmail.com>
2019-01-30 11:21:07 -0600

    Changed gulpfiles to work with gulp 4.0  Changes to be committed: 	modified:   mobile_portfolio/gulpfile.js 	new file:   package-lock.json 	modified:   package.json 	modified:   pizza_app/gulpfile.js

--- mobile_portfolio/gulpfile.js
+++ mobile_portfolio/gulpfile.js
@@ -1,4 +1,4 @@
-#!/bin/env gulp
+#!/bin/env node
 
 // gulpfile.js for Mobile Portfolio
 
@@ -10,100 +10,100 @@ var cleancss = require('gulp-cleancss');
 var gm = require('gulp-gm');
 var mustache = require('gulp-mustache');
 
-gulp.task('clean', function() {
-    return del.sync('dist/*')
+gulp.task('clean', async function() {
+  return del.sync('dist/*')
 });
 
-gulp.task('minify_scripts', ['clean'], function() {
+gulp.task('minify_scripts', function() {
 	return gulp.src('src/js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js/'));
+		  .pipe(uglify())
+		  .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('minify_stylesheets', ['clean'], function() {
+gulp.task('minify_stylesheets', function() {
 	return gulp.src('src/css/*.css')
-		.pipe(cleancss())
-		.pipe(gulp.dest('dist/css/'));
+		  .pipe(cleancss())
+		  .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('images_min', ['clean'], function() {
-    return gulp.src('src/img/profilepic.jpg')
-        .pipe(gm(function(gmfile){
-            return gmfile
-                .resize(70,70)
-                .strip()
-                //.gaussian('0.05')
-                .interlace('Plane')
-                .quality('80%');
-        }))
-        .pipe(gulp.dest('dist/img/'));
+gulp.task('images_min', function() {
+  return gulp.src('src/img/profilepic.jpg')
+      .pipe(gm(function(gmfile){
+        return gmfile
+            .resize(70,70)
+            .strip()
+        //.gaussian('0.05')
+            .interlace('Plane')
+            .quality('80%');
+      }))
+      .pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('copy_images', ['clean'], function() {
-    return gulp.src(['src/img/*', '!src/img/profilepic.jpg'])
-        .pipe(gulp.dest('dist/img/'));
+gulp.task('copy_images', function() {
+  return gulp.src(['src/img/*', '!src/img/profilepic.jpg'])
+      .pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('render_index_html', ['minify_stylesheets', 'minify_scripts'], function() {
+gulp.task('render_index_html', function() {
+  
+  var hash = {
+    // inlined stylesheets
+    open_sans_css: '<style>'+ fs.readFileSync('dist/css/open-sans.css') +'</style>',
+    style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
+    print_css: '<style>'+ fs.readFileSync('dist/css/print.css') +'</style>',
     
-    var hash = {
-        // inlined stylesheets
-        open_sans_css: '<style>'+ fs.readFileSync('dist/css/open-sans.css') +'</style>',
-        style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
-        print_css: '<style>'+ fs.readFileSync('dist/css/print.css') +'</style>',
-        
-        // inlined scripts
-        analitics_profile_js: '<script>' + fs.readFileSync('dist/js/analytics_profile.js') + '</script>',
-        perfmatters_js: '<script>' + fs.readFileSync('dist/js/perfmatters.js') + '</script>',
+    // inlined scripts
+    analitics_profile_js: '<script>' + fs.readFileSync('dist/js/analytics_profile.js') + '</script>',
+    perfmatters_js: '<script>' + fs.readFileSync('dist/js/perfmatters.js') + '</script>',
 
-        // asynchronous script load
-        analytics_js: '<script async src="//www.google-analytics.com/analytics.js"></script>',
+    // asynchronous script load
+    analytics_js: '<script async src="//www.google-analytics.com/analytics.js"></script>',
 
-        // relative paths to images 
-        profilepic: '<img class="header" src="img/profilepic.jpg" alt="img/profilepic.jpg">',
-    };
+    // relative paths to images 
+    profilepic: '<img class="header" src="img/profilepic.jpg" alt="img/profilepic.jpg">',
+  };
 
 	return gulp.src('src/html/index.html')
-        .pipe(mustache(hash))
-		.pipe(gulp.dest('dist/'));
+      .pipe(mustache(hash))
+		  .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('render_other_html', ['minify_stylesheets', 'minify_scripts'], function() {
+gulp.task('render_other_html', function() {
 
-    var hash = {
+  var hash = {
 
-        // inlined stylesheets
-        open_sans_css: '<style>'+ fs.readFileSync('dist/css/open-sans.css') +'</style>',
-        style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
-        print_css: '<style>'+ fs.readFileSync('dist/css/print.css') +'</style>',
+    // inlined stylesheets
+    open_sans_css: '<style>'+ fs.readFileSync('dist/css/open-sans.css') +'</style>',
+    style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
+    print_css: '<style>'+ fs.readFileSync('dist/css/print.css') +'</style>',
 
-        // inlined scripts
-        analitics_profile_js: '<script>' + fs.readFileSync('dist/js/analytics_profile.js') + '</script>',
-        perfmatters_js: '<script>' + fs.readFileSync('dist/js/perfmatters.js') + '</script>',
+    // inlined scripts
+    analitics_profile_js: '<script>' + fs.readFileSync('dist/js/analytics_profile.js') + '</script>',
+    perfmatters_js: '<script>' + fs.readFileSync('dist/js/perfmatters.js') + '</script>',
 
-        // asynchronous script load
-        analytics_js: '<script async src="//www.google-analytics.com/analytics.js"></script>',
+    // asynchronous script load
+    analytics_js: '<script async src="//www.google-analytics.com/analytics.js"></script>',
 
-        // relative paths to images 
-        profilepic: '<img class="header" src="../img/profilepic.jpg" alt="profilepic.jpg">',
+    // relative paths to images 
+    profilepic: '<img class="header" src="../img/profilepic.jpg" alt="profilepic.jpg">',
 		mobilewebdev_jpg: '<img src="../img/mobilewebdev.jpg" alt="mobilewebdev.jpg">',
-        cam_be_like_jpg: '<img src="../img/cam_be_like.jpg" alt="cam_be_like.jpg">',
-    };
+    cam_be_like_jpg: '<img src="../img/cam_be_like.jpg" alt="cam_be_like.jpg">',
+  };
 
 	return gulp.src(['src/html/*', '!src/html/index.html'])
-        .pipe(mustache(hash))
-		.pipe(gulp.dest('dist/projects'));
+      .pipe(mustache(hash))
+		  .pipe(gulp.dest('dist/projects'));
 });
 
-gulp.task('default', [
-    'clean',
-    'minify_scripts',
-    'minify_stylesheets',
-    'copy_images',
-    'images_min',
-    'render_index_html',
-    'render_other_html',
-]);
+gulp.task('default', gulp.series(
+  'clean',
+  'minify_scripts',
+  'minify_stylesheets',
+  'copy_images',
+  'images_min',
+  'render_index_html',
+  'render_other_html',
+));
 
 
 
--- pizza_app/gulpfile.js
+++ pizza_app/gulpfile.js
@@ -1,4 +1,4 @@
-#!/bin/env gulp
+#!/bin/env node
 
 // gulpfile.js for Pizza App
 
@@ -13,130 +13,130 @@ var concat = require('gulp-concat');
 var gm = require('gulp-gm');
 var rename = require('gulp-rename');
 
-gulp.task('clean', function() {
-    return del.sync('dist/*');
+gulp.task('clean', async function() {
+  return del.sync('dist/*');
 });
 
 // JavaScript tasks
 
-gulp.task('scripts', ['clean'], function() {
-	 return gulp.src('src/js/*.js')
-            .pipe(concat('main.js'))
-		      .pipe(gulp.dest('dist/js/'));
-});
+gulp.task('scripts', gulp.series('clean', async function() {
+	return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+		  .pipe(gulp.dest('dist/js/'));
+}));
 
-gulp.task('scripts_min', ['clean'], function() {
-	 return gulp.src('src/js/*.js')
-            .pipe(concat('main.js'))
-		      .pipe(uglify())
-		      .pipe(gulp.dest('dist/js/'));
-});
+gulp.task('scripts_min', gulp.series('clean', async function() {
+	return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+		  .pipe(uglify())
+		  .pipe(gulp.dest('dist/js/'));
+}));
 
 // CSS tasks
 
-gulp.task('styles', ['clean'], function() {
-	 return gulp.src('src/css/*.css')
-		      .pipe(gulp.dest('dist/css/'));
-});
+gulp.task('styles', gulp.series('clean', async function() {
+	return gulp.src('src/css/*.css')
+		  .pipe(gulp.dest('dist/css/'));
+}));
 
-gulp.task('styles_min', ['clean'], function() {
-	 return gulp.src('src/css/*.css')
-		      .pipe(compress({
-				    type: 'css'
-		      }))
-		      .pipe(gulp.dest('dist/css/'));
-});
+gulp.task('styles_min', gulp.series('clean', async function() {
+	return gulp.src('src/css/*.css')
+		  .pipe(compress({
+				type: 'css'
+		  }))
+		  .pipe(gulp.dest('dist/css/'));
+}));
 
 // image tasks
 
-gulp.task('images', ['clean'], function() {
+gulp.task('images', gulp.series('clean', async function() {
 
-    gulp.src('src/images/pizza.png')
-            .pipe(gm(function(gmfile){
-                return gmfile.resize(180,233).quality('20%')
-            }))
-            .pipe(rename('menu_pizza.png'))
-            .pipe(gulp.dest('dist/images/'));
+  gulp.src('src/images/pizza.png')
+      .pipe(gm(function(gmfile){
+        return gmfile.resize(180,233).quality('20%')
+      }))
+      .pipe(rename('menu_pizza.png'))
+      .pipe(gulp.dest('dist/images/'));
 
-    gulp.src('src/images/pizza.png')
-            .pipe(gm(function(gmfile){
-                return gmfile.resize(77,100).quality('20%')
-            }))
-            .pipe(rename('sliding_pizza.png'))
-            .pipe(gulp.dest('dist/images/'));
+  gulp.src('src/images/pizza.png')
+      .pipe(gm(function(gmfile){
+        return gmfile.resize(77,100).quality('20%')
+      }))
+      .pipe(rename('sliding_pizza.png'))
+      .pipe(gulp.dest('dist/images/'));
 
-    gulp.src('src/images/pizzeria.jpg')
-            .pipe(gm(function(gmfile){
-                return gmfile
-                    .resize(360, 270)
-                    .strip()
-                    //.gaussian(0.05)
-                    .interlace('Plane')
-                    .quality('50%');
-            }))
-            .pipe(gulp.dest('dist/images/'));
+  gulp.src('src/images/pizzeria.jpg')
+      .pipe(gm(function(gmfile){
+        return gmfile
+            .resize(360, 270)
+            .strip()
+        //.gaussian(0.05)
+            .interlace('Plane')
+            .quality('50%');
+      }))
+      .pipe(gulp.dest('dist/images/'));
 
-//http://stackoverflow.com/questions/7261855/recommendation-for-compressing-jpg-files-with-imagemagick
-//convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% source.jpg result.jpg
+  //http://stackoverflow.com/questions/7261855/recommendation-for-compressing-jpg-files-with-imagemagick
+  //convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% source.jpg result.jpg
 
-    gulp.src('../pizza_app/src/images/pizzeria.jpg')
-            .pipe(gm(function(gmfile){
-                return gmfile
-                        .resize(100)
-                        .strip()
-                        .interlace('Plane')
-                        .quality('80%');
-            }))
-            .pipe(rename('pizzeria_sm.jpg'))
-            .pipe(gulp.dest('dist/images/'));
+  gulp.src('../pizza_app/src/images/pizzeria.jpg')
+      .pipe(gm(function(gmfile){
+        return gmfile
+            .resize(100)
+            .strip()
+            .interlace('Plane')
+            .quality('80%');
+      }))
+      .pipe(rename('pizzeria_sm.jpg'))
+      .pipe(gulp.dest('dist/images/'));
 
-    return true;
-});
+  return true;
+}));
 
 // HTML tasks
 
-gulp.task('templates', ['styles', 'scripts'], function() {
-    var pizza_template = fs.readFileSync("src/html/pizza_template.html", 'utf-8');
-	 gulp.src('src/html/index.html')
-            .pipe(mustache({
-                style_css: '<link rel="stylesheet" href="css/style.css">',
-                pizza_template: pizza_template,
-                mustache_js: '<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js"></script>',
-                main_js: '<script src="js/main.js"></script>'
-            }))
-		      .pipe(gulp.dest('dist/'));
-});
+gulp.task('templates', gulp.series('styles', 'scripts', async function() {
+  var pizza_template = fs.readFileSync("src/html/pizza_template.html", 'utf-8');
+	gulp.src('src/html/index.html')
+      .pipe(mustache({
+        style_css: '<link rel="stylesheet" href="css/style.css">',
+        pizza_template: pizza_template,
+        mustache_js: '<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js"></script>',
+        main_js: '<script src="js/main.js"></script>'
+      }))
+		  .pipe(gulp.dest('dist/'));
+}));
 
-gulp.task('templates_min', ['styles_min', 'scripts_min'], function() {
-    var pizza_template = fs.readFileSync("src/html/pizza_template.html", 'utf-8');
-	 gulp.src('src/html/index.html')
-            .pipe(mustache({
-                style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
-                pizza_template: pizza_template,
-                mustache_js: '<script async src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js"></script>',
-                main_js: '<script async src="js/main.js"></script>'
-            }))
-		      .pipe(gulp.dest('dist/'));
-});
+gulp.task('templates_min', gulp.series('styles_min', 'scripts_min', async function() {
+  var pizza_template = fs.readFileSync("src/html/pizza_template.html", 'utf-8');
+	gulp.src('src/html/index.html')
+      .pipe(mustache({
+        style_css: '<style>'+ fs.readFileSync('dist/css/style.css') +'</style>',
+        pizza_template: pizza_template,
+        mustache_js: '<script async src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js"></script>',
+        main_js: '<script async src="js/main.js"></script>'
+      }))
+		  .pipe(gulp.dest('dist/'));
+}));
 
-gulp.task('dist', [
-    //'clean',
-    'images',
-    'scripts_min',
-    'styles_min',
-    'templates_min'
-]);
+gulp.task('dist', gulp.series(
+  //'clean',
+  'images',
+  'scripts_min',
+  'styles_min',
+  'templates_min'
+));
 
 
-gulp.task('develop', [
-    'clean',
-    'images',
-    'scripts',
-    'styles',
-    'templates'
-]);
+gulp.task('develop', gulp.series(
+  'clean',
+  'images',
+  'scripts',
+  'styles',
+  'templates'
+));
 
-gulp.task('default', [
-    'develop'
-]);
+gulp.task('default', gulp.series(
+  'develop'
+));
 
